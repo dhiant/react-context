9778389c2dd2031bf68ccd2377f1407ee823a2bb
Sushant Dhimal <sushant07.dhimal@gmail.com>
2022-09-10 11:26:28 +0545

    chore: update title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <>
       <div className="container mx-auto">
-        <h3 className="text-center text-3xl mt-20  leading-8 text-black font-bold tracking-wide uppercase">
-          CRUD App with React Context API and Hooks
+        <h3 className="text-center text-emerald-600 text-3xl mt-20  leading-8 text-black font-bold tracking-wide uppercase">
+          StaffBook
         </h3>
         <Heading />
         <EmployeeList />
